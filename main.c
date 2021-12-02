358283cbec0ee74958ed9f5cce8a447153e960ac
JoonT0M26 <95414300+JoonT0M26@users.noreply.github.com>
2021-12-02 14:28:24 +0100

    begin knopen

--- main.c
+++ main.c
@@ -1,14 +1,36 @@
-/*
- */
-
 #include <avr/io.h>
+#define AAN PF1
+#define UIT PF2
+#define X PF1
+#define Y PF2
+#define MIN PF1
+#define PLUS PF1
+#define START PF1
+#define NOODSTOP PF2
 
 int main(void)
 {
+    DDRF &= ~(1 << AAN);
+    DDRF &= ~(1 << UIT);
+    DDRF &= ~(1 << X);
+    DDRF &= ~(1 << Y);
+    DDRF &= ~(1 << X);
+    DDRF &= ~(1 << Y);
+    DDRF &= ~(1 << MIN);
+    DDRF &= ~(1 << PLUS);
+    DDRF &= ~(1 << START);
+    DDRF &= ~(1 << NOODSTOP);
+
 
     // Insert code
 
     while(1)
+    {
+        if(!(PINF & (1 << AAN)))
+        {
+
+        }
+    }
     ;
 
     return 0;
@@ -23,4 +45,10 @@ int main(void)
 //z-as motor aan
 //z-as motor uit als drukschakelaar in ingedrukt
 //elektromagneet aan
-//
+
+//motor y-as aan adere kant op draaien
+//motor x-as draaien
+//motor y-as draaien
+//motor z-as draaien
+//elektromagneet uit
+//motor y-as draaien
