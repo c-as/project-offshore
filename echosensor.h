fd76f430c7370f50bd5355abe9e1b3a28179ad8d
Cas Brugman <casbrugman@hotmail.nl>
2021-12-14 16:07:21 +0100

    Afstandssensor gefixed (nog geen eenheid tho)

--- echosensor.h
+++ echosensor.h
@@ -6,56 +6,54 @@
 #define ECHO_HOOG PINA & (1 << 3)
 
 int echosensor_ticks = 0;
-int echosensor_mm = 0;
-int echosensor_us = 0;
-int timer1_beginwaarde = 63936;
 
-ISR(TIMER1_OVF_vect){
-    static int ticks = 0;
-    static bool uitgezonden = false;
-    static bool echo = false;
-    if(uitgezonden){
-        if(ECHO_HOOG){
-            if(echo){
-                ticks++;
-            } else {
-                echo = true;
-            }
-        } else {
-            if (echo){
-                uitgezonden = false;
-                echo = false;
-                echosensor_us = ticks * 100;
-                echosensor_mm = echosensor_us / 6;
-                echosensor_ticks = ticks;
-                ticks = 0;
-            }
+bool echo_last = false;
+bool echo = false;
+bool send = false;
+
+ISR(INT0_vect){
+    bool echo_current = ECHO_HOOG;
+    if(echo_current != echo_last){
+        if(echo_current && !echo){
+            //begin met tellen
+            TCNT1 = 0;
+            echo = true;
         }
-    } else {
-        TRIG_LAAG;
-        _delay_us(5);
-        TRIG_HOOG;
-        _delay_us(10);
-        TRIG_LAAG;
-        uitgezonden = true;
+        if(!echo_current && echo){
+            //stop met tellen
+            echosensor_ticks = TCNT1;
+            echo = false;
+            send = false;
+        }
+        echo_last = echo_current;
     }
-
-	TCNT1 = timer1_beginwaarde;
 }
 
 void init_echosensor(){
-    cli();
-    DDRE |= (1 << 3);
-    DDRG &= ~(1 << 5);
+    DDRA |= (1 << 2);
+    DDRA &= ~(1 << 3);
+
+    EIMSK |= (1 << INT0);
 
     //timer 1 16 bit
-    //zet beginwaarde
-    TCNT1 = timer1_beginwaarde;
     //mode 0
 	TCCR1A = 0;
-	//div 64
-	TCCR1B = (0<<CS02) | (1<<CS01) | (1<<CS00);
-    //enable overflow interrupt
-	TIMSK1 = (1<<TOIE0);
+	//div 1024
+	TCCR1B |= (1<<CS12) | (0<<CS11) | (1<<CS10);
+
 	sei();
+}
+
+void check_echosensor(){
+    if(send){
+        return;
+    }
+
+    TRIG_LAAG;
+    _delay_us(5);
+    TRIG_HOOG;
+    _delay_us(15);
+    TRIG_LAAG;
+
+    send = true;
 }
