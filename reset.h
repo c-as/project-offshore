add6d3dfd1ed1e2a4c072e2e1faee040ec7b507f
Cas Brugman <casbrugman@hotmail.nl>
2022-01-24 16:43:40 +0100

    Beetje omgegooid

--- reset.h
+++ reset.h
@@ -4,43 +4,55 @@
 
 void reset()
 {
-    LED_BEZIG_AAN;
-
     zet_motor(100, MOTOR_Z);
+
+    LED_AAN_AAN;
 
     while(1)
     {
         if(Z_AS_END_SWITCH_BOVEN_INGEDRUKT)
         {
-            zet_motor(0, MOTOR_Z);
-
-            zet_motor(100, MOTOR_X);
             break;
         }
     }
+
+    LED_MAGNEET_AAN;
+
+    zet_motor(100, MOTOR_X);
+
+    zet_motor(100, MOTOR_X);
 
     while(1)
     {
         if(X_AS_END_SWITCH_INGEDRUKT)
         {
-            zet_motor(0, MOTOR_X);
-
-            zet_motor(100, MOTOR_Y);
-
-            rotoencoder_clock_x = 0;
             break;
         }
     }
+
+    LED_BEZIG_AAN;
+
+    zet_motor(0, MOTOR_X);
+
+    zet_motor(100, MOTOR_Y);
+
+    rotoencoder_clock_x = 0;
 
     while(1)
     {
         if(Y_AS_END_SWITCH_INGEDRUKT)
         {
-            zet_motor(0, MOTOR_Y);
 
-            rotoencoder_clock_y = 0;
             break;
         }
     }
-    return 0;
+
+    LED_GEREED_AAN;
+
+    zet_motor(0, MOTOR_Y);
+
+    rotoencoder_clock_y = 0;
+
+
+    return;
 }
