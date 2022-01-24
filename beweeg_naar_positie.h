9e517e07ae431eb52ab42fed55cc990dccd617ce
Cas Brugman <casbrugman@hotmail.nl>
2022-01-25 00:00:20 +0100

    Beweeg naar positie volledig

--- beweeg_naar_positie.h
+++ beweeg_naar_positie.h
@@ -1,26 +1,34 @@
 
 //beweeg naar positie
 
-void beweeg_naar_positie()
+void beweeg_naar_positie(struct coordinaat coord)
 {
-    zet_motor(100, MOTOR_X);
+    if(coord.x > rotoencoder_clock_x){
+        zet_motor(100, MOTOR_X);
+    } else {
+        zet_motor(-100, MOTOR_X);
+    }
 
     while(1)
     {
-        if(1) //x rotoencoder positie gelijk aan doel x positie
+        if(rotoencoder_clock_x == coord.x) //x rotoencoder positie gelijk aan doel x positie
         {
             zet_motor(0, MOTOR_X);
             break;
         }
     }
 
-    zet_motor(100, MOTOR_Y);
+    if(coord.y > rotoencoder_clock_y){
+        zet_motor(100, MOTOR_Y);
+    } else {
+        zet_motor(-100, MOTOR_Y);
+    }
 
     while(1)
     {
-        if(1) //y rotoencoder positie gelijk aan doel y positie
+        if(rotoencoder_clock_y == coord.y) //y rotoencoder positie gelijk aan doel y positie
         {
-            zet_motor(100, MOTOR_Y);
+            zet_motor(0, MOTOR_Y);
             break;
         }
     }
