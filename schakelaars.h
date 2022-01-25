8d707fb9d20a019a402c942512b89c468dbaa7ac
Cas Brugman <casbrugman@hotmail.nl>
2022-01-25 01:14:33 +0100

    Code voor magneet

--- schakelaars.h
+++ schakelaars.h
@@ -3,6 +3,7 @@
 //X-as end switch Digital36 PC1
 //Z-as end switch boven Digital35 PC2
 //Z-as end switch onder Digital34 PC3
+//magneet Digital38 PD7
 
 #define NOODSTOP_INGEDRUKT (PINL & (1 << 6))
 #define Y_AS_END_SWITCH_INGEDRUKT (!(PINC & (1 << 0)))
@@ -10,10 +11,15 @@
 #define Z_AS_END_SWITCH_BOVEN_INGEDRUKT (!(PINC & (1 << 2)))
 #define Z_AS_END_SWITCH_ONDER_INGEDRUKT (!(PINC & (1 << 3)))
 
+#define MAGNEET_AAN PORTD |= (1 <<7)
+#define MAGNEET_UIT PORTD &= ~(1 << 7)
+
 void init_schakelaars(){
     //zet input
     DDRL &= ~(1 << 6);
     DDRC &= ~((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3));
+    //zet output
+    DDRD |= (1 << 7);
     //enable pullup
     PORTL |= (1 << 6);
     PORTC |= ((1 << 0) | (1 << 1) | (1 << 2) | (1 << 3));
